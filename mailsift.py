f69cb4bab8c9f2659e4b44caec429ba206bcee25
Abhay-1552 <gamerspropaganda143@gmail.com>
2024-02-04 23:27:40 +0530

    Working

--- mailsift.py
+++ mailsift.py
@@ -30,7 +30,7 @@ class Mail:
             status, messages = self.mail.search(None, "ALL")
             messages = messages[0].split()
             messages.reverse()
-            top_messages = messages[:20]
+            top_messages = messages[:10]
 
             filtered_email_list = []
 
@@ -44,6 +44,8 @@ class Mail:
 
                 # Get sender's name, email address, subject, date, and time
                 sender_name, sender_email = email.utils.parseaddr(email_message["From"])
+                sender_name = sender_name or "Unknown Sender"
+                sender_email = sender_email or "unknown@example.com"
 
                 # Decode subject with a default encoding if it's None
                 subject, encoding = decode_header(email_message["Subject"])[0]
@@ -56,23 +58,42 @@ class Mail:
                 time_received = date_time_received.strftime("%H:%M")
 
                 payload = email_message.get_payload()
-                decoded_payload = ''
+                decoded_payload = " "
 
-                # Get the body of the email
+                # Get the body of the email, prioritizing text/plain parts
                 if email_message.is_multipart():
                     for part in payload:
-                        if part.get_content_type() == "text/plain":
-                            decoded_payload = part.get_payload(decode=True).decode(part.get_content_charset() or "utf-8", errors="ignore")
-                            break
-                        elif part.get_content_type() == "text/html":
-                            html_payload = part.get_payload()
-                            soup = BeautifulSoup(html_payload, 'html.parser')
-                            decoded_payload = soup.get_text()
-                elif email_message.get_content_type() == "text/plain":
-                    decoded_payload = payload.decode(email_message.get_content_charset() or "utf-8", errors="ignore")
+                        content_type = part.get_content_type()
+                        if content_type == "text/plain":
+                            decoded_payload += part.get_payload(decode=True).decode(
+                                part.get_content_charset() or "utf-8", errors="ignore")
 
-                # Clean URLs from the payload
-                cleaned_payload = re.sub(r'http\S+', '', decoded_payload)
+                        elif content_type == "text/html":
+                            html_payload = part.get_payload(decode=True)  # Decode HTML payload
+                            soup = BeautifulSoup(html_payload, 'html.parser')
+                            decoded_payload += soup.get_text()
+
+                        elif part.get_content_type().startswith('image/'):
+                            filename = part.get_filename()
+                            decoded_payload += f"Image Attachment: {filename}, Content Type: {part.get_content_type()}\t"
+
+                else:
+                    if email_message.get_content_type() == "text/plain":
+                        decoded_payload += payload.decode(email_message.get_content_charset() or "utf-8", errors="ignore")
+
+                    elif email_message.get_content_type() == "text/html":
+                        html_payload = email_message.get_payload(decode=True)
+                        soup = BeautifulSoup(html_payload, 'html.parser')
+                        decoded_payload += soup.get_text()
+
+                    elif email_message.get_content_type().startswith('image/'):
+                        attachment_filename = email_message.get_filename()
+                        decoded_payload += f"Image Attachment: {attachment_filename}, Content Type: {email_message.get_content_type()}\t"
+
+                    else:
+                        decoded_payload += "Unsupported Content or Blank Mail"
+
+                cleaned_payload = Mail.cleaning_mail(decoded_payload)
 
                 email_info = {
                     "SenderName": sender_name,
@@ -86,18 +107,33 @@ class Mail:
                 print(email_info)
                 print('-' * 200)
 
-                # Add the dictionary to the list
                 filtered_email_list.append(email_info)
 
-            # Output the filtered information in JSON format
             output_json = json.dumps(filtered_email_list, indent=2)
-            print(output_json)
 
             Mail.mail_to_excel(output_json)
 
+        except (KeyboardInterrupt, Exception, ConnectionError) as e:
+            print(f"Error: {e}")
+
         finally:
-            # Logout from the server
             self.mail.logout()
+
+    @staticmethod
+    def cleaning_mail(payload):
+        # Cleaning - Hyperlinks
+        cleaned_payload = re.sub(r'http\S+', '', payload)
+        # Cleaning - Unicode Characters
+        cleaned_payload = re.sub(r'[^\x00-\x7F]+', '', cleaned_payload)
+        # Cleaning - Unnecessary Punctuations
+        cleaned_payload = re.sub(r'[^\w\s.,-?:;]', '', cleaned_payload)
+        # Cleaning - Repeated unnecessary character
+        cleaned_payload = re.sub(r'([^\w\s])\1+', r'\1', cleaned_payload)
+
+        if cleaned_payload.isspace():
+            cleaned_payload += "Blank Mail"
+
+        return cleaned_payload
 
     @staticmethod
     def mail_to_excel(email_json_data):
