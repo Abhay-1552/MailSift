a810e28c4c94cd47a5fc1dd0bf835090ce1ec62e
Abhay-1552 <patelabhay1552@gmail.com>
2024-03-13 18:41:46 +0530

    Working

---
+++ login.py
@@ -0,0 +1,38 @@
+import psycopg2
+from urllib.parse import urlparse
+from psycopg2 import extensions
+from psycopg2 import sql
+from dotenv import load_dotenv
+import os
+
+
+class AnimeDB:
+    def __init__(self):
+        self.url = urlparse(os.getenv('URL'))
+
+        # Connect to the CockroachDB database using the URL
+        # self.conn = psycopg2.connect(
+        #     database=self.url.path[1:],
+        #     user=self.url.username,
+        #     password=self.url.password,
+        #     host=self.url.hostname,
+        #     port=self.url.port
+        # )
+
+        self.conn = psycopg2.connect(self.url)
+
+        try:
+            if self.conn.status == extensions.STATUS_READY:
+                print("Connection Successful!")
+
+        except Exception as e:
+            print("Connection Failed:", e)
+
+    def close_connection(self):
+        if hasattr(self, 'conn') and self.conn is not None:
+            self.conn.close()
+            print("Connection Closed")
+
+
+if __name__ == '__main__':
+    AnimeDB()
